344a5f3a5f2e77df798e4647b326f56033aa4cb1
Dana <danaedwards0206@gmail.com>
2023-08-09 16:01:57 -0500

    created endpoint for retrieving steps data

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const { google } = require('googleapis')
+const axios = require('axios')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+// Configure Google Fit API credentials
+const credentials = require('./Auth.json')
+const { client_secret, client_id, redirect_uris } = credentials.web
+const oAuth2Client = new google.auth.OAuth2(
+  client_id,
+  client_secret,
+  redirect_uris[0]
+)
+
+// Define API endpoints
+app.get('/auth', (req, res) => {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: ['https://www.googleapis.com/auth/fitness.activity.read']
+  })
+
+  res.redirect(authUrl)
+})
+
+app.get('/callback', async (req, res) => {
+  const code = req.query.code
+  const { tokens } = await oAuth2Client.getToken(code)
+  oAuth2Client.setCredentials(tokens)
+
+  // Redirect the user to the /steps endpoint with the access token as a query parameter
+  res.redirect(`/steps?access_token=${tokens.access_token}`)
+})
+
+app.get('/steps', async (req, res) => {
+  try {
+    const accessToken = req.query.access_token
+
+    // Use the provided access token for authentication
+    oAuth2Client.setCredentials({ access_token: accessToken })
+
+    const fitness = google.fitness('v1')
+    const response = await fitness.users.dataset.aggregate({
+      userId: 'me',
+      requestBody: {
+        aggregateBy: [{ dataTypeName: 'com.google.step_count.delta' }],
+        bucketByTime: { durationMillis: 86400000 }, // 24 hours
+        startTimeMillis: Date.now() - 7 * 24 * 60 * 60 * 1000, // 7 days ago
+        endTimeMillis: Date.now()
+      },
+      auth: oAuth2Client
+    })
+    const stepsData =
+      response.data.bucket[0].dataset[0].point[0].value[0].intVal
+    res.json({ steps: stepsData })
+  } catch (error) {
+    console.error('Error retrieving steps data:', error.message)
+    res.status(500).json({ error: 'Error retrieving steps data' })
+  }
+})
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
